acff9b08f50badb3d9c23e559bb7ca153d7a8280
Stacy-from-Mars <stacy.from.mars@gmail.com>
2024-07-28 12:06:31 +0300

    step 58

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-    const sorted = array.sort((a, b) => a - b);
+    const sorted = array.slice().sort((a, b) => a - b);
     const median = sorted.length % 2 === 0 ?
         getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]]) :
         sorted[Math.floor(sorted.length / 2)];
